7bdb6c80192b23903c770bad8aa18dff45933190
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-04 15:03:35 +0200

    Refactor one theory testing one behaviour

--- tests/PricingKata.Tests/UnitTest1.cs
+++ tests/PricingKata.Tests/UnitTest1.cs
@@ -2,26 +2,12 @@ namespace PricingKata.Tests;
 
 public class UnitTest1
 {
-    // 3 articles à 1,21 € et taxe 0 % → “3.63 €”
-    [Fact]
-    public void Test1()
-    {
-        GetPrice(3, 1.21m, 0).Should().Be("3.63 €");
-    }
-    
-    // 3 articles à 1,21 € et taxe 5 % → “3.81 €”
-    [Fact]
-    public void Test2()
-    {
-        GetPrice(3, 1.21m, 5).Should().Be("3.81 €");
-    }
-    
-    // 3 articles à 1,21 € et taxe 20 % → “4.36 €”
-    [Fact]
-    public void Test3()
-    {
-        GetPrice(3, 1.21m, 20).Should().Be("4.36 €");
-    }
+    [Theory]
+    [InlineData(3, 1.21, 0, "3.63 €")]
+    [InlineData(3, 1.21, 5, "3.81 €")]
+    [InlineData(3, 1.21, 20, "4.36 €")]
+    public void Test1(int itemCount, decimal itemPrice, int taxRateInPercentage, string expectedPrice)
+        => GetPrice(itemCount, itemPrice, taxRateInPercentage).Should().Be(expectedPrice);
 
     private string GetPrice(int itemCount, decimal itemPrice, int taxRateInPercentage)
     {
@@ -31,15 +17,11 @@ public class UnitTest1
     }
 
     private static decimal ApplyTax(decimal priceWithoutTax, int taxRateInPercentage)
-    {
-        return priceWithoutTax * ConvertTaxRate(taxRateInPercentage);
-    }
+        => priceWithoutTax * ConvertTaxRate(taxRateInPercentage);
 
     private static decimal ConvertTaxRate(decimal taxRateInPercentage)
         => taxRateInPercentage / 100m + 1;
 
     private static string ToString(decimal price)
-    {
-        return FormattableString.Invariant($"{price:0.00} €");
-    }
+        => FormattableString.Invariant($"{price:0.00} €");
 }
