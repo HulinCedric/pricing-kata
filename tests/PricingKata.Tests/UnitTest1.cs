bb4eae0390285a9079e76559d15980e07d8e0d93
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-04 14:14:30 +0200

    Fake It

--- tests/PricingKata.Tests/UnitTest1.cs
+++ tests/PricingKata.Tests/UnitTest1.cs
@@ -8,9 +8,17 @@ public class UnitTest1
     {
         GetPrice(3, 1.21m, 0).Should().Be("3.63 €");
     }
+    
+    // 3 articles à 1,21 € et taxe 5 % → “3.81 €”
+    [Fact]
+    public void Test2()
+    {
+        GetPrice(3, 1.21m, 5).Should().Be("3.81 €");
+    }
 
     private string GetPrice(int itemCount, decimal itemPrice, int taxRateInPercentage)
     {
+        if (taxRateInPercentage == 5) return "3.81 €";
         return "3.63 €";
     }
 }
