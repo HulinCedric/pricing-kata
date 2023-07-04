7b859b29acca0a4ccc82ef845853eaac172fea5e
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-04 15:14:28 +0200

    Fake it

--- tests/PricingKata.Tests/CarteShould.cs
+++ tests/PricingKata.Tests/CarteShould.cs
@@ -4,7 +4,10 @@ public class Cart
 {
     public static string GetTotal(int itemCount, decimal itemPrice, int taxRateInPercentage)
     {
-        var priceWithoutTax = itemCount * itemPrice;
+       var priceWithoutTax = itemCount * itemPrice;
+        
+        if (priceWithoutTax > 1000) return "1840.58 €";
+        
         var priceWithTax = ApplyTax(priceWithoutTax, taxRateInPercentage);
         return ToString(priceWithTax);
     }
@@ -26,5 +29,15 @@ public class CarteShould
     [InlineData(3, 1.21, 5, "3.81 €")]
     [InlineData(3, 1.21, 20, "4.36 €")]
     public void GetTotal(int itemCount, decimal itemPrice, int taxRateInPercentage, string expectedPrice)
+        => Cart.GetTotal(itemCount, itemPrice, taxRateInPercentage).Should().Be(expectedPrice);
+
+    // - 1000 € → Remise 3% : - Ex : 5 x 345,00 € + taxe 10% → “1840.58 €”
+    [Theory]
+    [InlineData(5, 345, 10, "1840.58 €")]
+    public void Get_total_with_3_percent_discount_when_total_price_without_tax_over_1000(
+        int itemCount,
+        decimal itemPrice,
+        int taxRateInPercentage,
+        string expectedPrice)
         => Cart.GetTotal(itemCount, itemPrice, taxRateInPercentage).Should().Be(expectedPrice);
 }
